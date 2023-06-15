bc6545930f5ce660662f417feff650db1691f0ac
rluki99 <120097849+rluki99@users.noreply.github.com>
2023-06-15 20:13:43 +0200

    First Commit

---
+++ src/components/Cards.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { GENDER, type Invitee } from '../types'
+
+defineProps<{
+	invitee: Invitee
+}>()
+</script>
+
+<template>
+	<div :style="{ backgroundColor: invitee.gender === GENDER.MALE ? 'rgb(8,111,111)' : 'rgb(111,18,8)' }">
+		{{ invitee.name }}
+	</div>
+</template>
+
+<style scoped>
+div {
+	background-color: aqua;
+	border-radius: 5px;
+	margin-top: 5px;
+	text-align: center;
+	padding: 3px;
+	color: white;
+}
+</style>
